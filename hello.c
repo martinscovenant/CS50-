c2326171202cd9b7d41cc2b5e1673d70d4ac3384
martinscovenant <martinscovenant1@gmail.com>
2024-08-21 20:00:27 +0100

    print hello world

--- hello.c
+++ hello.c
@@ -2,8 +2,6 @@
 
 int main(void)
 {
-    char answer[100];  // Declare a character array to hold the string
-    printf("what's your name? ");
-    scanf("%99s", answer);  // Read the input string
-    printf("hello, %s\n", answer);
+    printf("hello, world\n");
+     printf("hello, world\n");
 }
