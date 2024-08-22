5fa10e3f4d8231ea0a67f662d5e7834f5958139c
martinscovenant <martinscovenant1@gmail.com>
2024-08-22 20:22:22 +0100

    cs50 else if

--- compare.c
+++ compare.c
@@ -4,13 +4,17 @@
 int main(void)
 {
     int x = get_int("what's x? ");
-    printf("You entered: %d\n", x);
-
     int y = get_int("what's y? ");
-    printf("You entered: %d\n", y);
 
     if (x < y) 
     {
         printf("x is less than y\n");
+    }
+    else if (x > y)
+    {
+        printf("x is greater than y\n");
+    }
+    else if (x == y){
+        printf("x is equal to y\n");
     }
 }
