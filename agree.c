b699894c27b6ead4163a516ca1939571966a02d5
martinscovenant <martinscovenant1@gmail.com>
2024-08-24 13:26:51 +0100

    agree

---
+++ agree.c
@@ -0,0 +1,16 @@
+#include <cs50.h>agre
+#include <stdio.h>
+
+int main(void)
+{
+   char c = get_char("Do you agree? ");
+
+   if (c == 'y' || c == 'Y')
+   {
+    printf("Agreed.\n");
+   }
+   else if (c == 'n' || c == 'N')
+   {
+    printf("Not agreed.\n");
+   }
+}
